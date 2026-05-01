6f7f3a06d79cd30c2cbd71f83c3dc1a3ca6c0d4a
Leila Wynn <leilawynn@Leilas-MacBook-Air.local>
2026-04-30 21:27:58 -0600

    I did this to myself. I forgot to save and accidentally deleted multiple files. Worst part is I didn't commit until now. :( I did this to myself because I'm forgetful.

---
+++ data.sql
@@ -0,0 +1,11 @@
+INSERT INTO characters (name, age, origin, current_status, stability_level) VALUES
+('Madoka Kaname', 14, 'Mitakihara City', 'Active', 90),
+('Homura Akemi', 14, 'Mitakihara City', 'Unstable', 65),
+('Sayaka Miki', 14, 'Mitakihara City', 'Critical', 35),
+('Mami Tomoe', 15, 'Mitakihara City', 'Active', 82),
+('Kyoko Sakura', 15, 'Kazamino City', 'Active', 75),
+('Nagisa Momoe', 12, 'Mitakihara City', 'Active', 70),
+('Iroha Tamaki', 15, 'Takarazaki City', 'Active', 85),
+('Yachiyo Nanami', 19, 'Kamihama City', 'Active', 78),
+('Tsuruno Yui', 17, 'Kamihama City', 'Active', 88),
+('Mifuyu Azusa', 18, 'Kamihama City', 'Unstable', 60);
